8ef99f62374bd341aa16779045b21868a65100fe
Olga <belaya-o.v@yandex.by>
2021-06-16 22:23:01 +0300

    21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.names}>
+                <div className={s.dialog}>
+                    Nick
+                </div>
+                <div className={s.dialog}>
+                    David
+                </div>
+                <div className={s.dialog}>
+                    Tom
+                </div>
+                <div className={s.dialog}>
+                    Jerry
+                </div>
+                <div className={s.dialog}>
+                    Anna
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
