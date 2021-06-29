17e83b349abd4648e4ff3b3b6841f2043a88a101
Olga <belaya-o.v@yandex.by>
2021-06-29 14:46:47 +0300

    messageBox-visibility

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -8,12 +8,21 @@ type MessagePropsType = {
 }
 
 function Message(props: MessagePropsType) {
+
+    let classMessageFrom = '.classMessageFrom';
+
     return (
         <div className={s.messageComponent}>
             <img className={s.img} src={props.img} alt="avatar"/>
-            <div className={s.messageBox}>
-                <div className={s.message}>{props.message}</div>
-                <div className={s.time}>22:00</div>
+            <div className={s.messageBackground}>
+                <div className={s.tail}/>
+                {/*<div className={`${s.messageBox} ${classMessageFrom}`}> */}
+                {/* как передать сюда еще один класс classMessageFrom
+                    (в браузере класс прописывается, но св-ва класса не применяются)*/}
+                <div className={s.messageBox}>
+                    <div className={s.message}>{props.message}</div>
+                    <div className={s.time}>22:00</div>
+                </div>
             </div>
         </div>
     )
