748817fbd21c4035b6a168f18af566f561d6c6a8
Olga <belaya-o.v@yandex.by>
2021-07-04 15:09:08 +0300

    37

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,27 @@
 import reportWebVitals from './reportWebVitals';
-import {renderEntireTree} from "./render";
-import state from "./redux/state";
 import './index.css';
+import ReactDOM from "react-dom";
+import React from "react";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+import store from "./redux/state";
 
-renderEntireTree(state);
+
+const renderEntireTree = (state: any) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state}
+                     updateNewPostMessage={store.updateNewPostMessage.bind(store)} addPost={store.addPost.bind(store)} removePost={store.removePost.bind(store)}
+                     addMessage={store.addMessage.bind(store)} updateNewMessage={store.updateNewMessage.bind(store)}/>
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+renderEntireTree(store.getter());
+
+store.subscribe(renderEntireTree);
 
 reportWebVitals();
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,65 +1,105 @@
-import {renderEntireTree} from "../render";
+const store = {
+    _renderEntireTree (_state: any) {
+        console.log(_state);
+    },
+    subscribe (observer: any) {
+        this._renderEntireTree = observer;
+    },
 
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'It is my first post!', likesCount: 25},
-            {id: 2, message: 'How are you?', likesCount: 15},
-        ],
-        newPostMessage: ''
+    getter() {
+        return this._state
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Nick', img: 'https://pixelbox.ru/wp-content/uploads/2020/11/ava-maincraft-youtube-76.jpg'},
-            {id: 2, name: 'Jhon', img: 'https://vraki.net/sites/default/files/inline/images/30_55.jpg'},
-            {id: 3, name: 'Anna', img: 'https://pixelbox.ru/wp-content/uploads/2021/04/ava-mult-vk-78.jpg'},
-        ],
-        messages: [
-            {message: 'Hi!', name: 'Nick', img: 'https://pixelbox.ru/wp-content/uploads/2020/11/ava-maincraft-youtube-76.jpg'},
-            {message: 'How are you?', name: 'Nick', img: 'https://pixelbox.ru/wp-content/uploads/2020/11/ava-maincraft-youtube-76.jpg'},
-            {message: 'Yo', name: 'Me', img: 'ava.jpg'},
-            {message: "I'm fine! How're you?", name: 'Me', img: 'ava.jpg'},
-        ],
-        newMessage: ''
+    setter(_state: any) {
+        this._state = _state;
     },
-    friendsPage: {
-        friends: [
-            {id: 1, name: 'Jhon', img: 'https://vraki.net/sites/default/files/inline/images/30_55.jpg'},
-            {id: 2, name: 'Merry', img: 'https://pixelbox.ru/wp-content/uploads/2021/02/mult-ava-instagram-2.jpg'},
-            {id: 3, name: 'Anna', img: 'https://pixelbox.ru/wp-content/uploads/2021/04/ava-mult-vk-78.jpg'},
-            {id: 4, name: 'Nick', img: 'https://pixelbox.ru/wp-content/uploads/2020/11/ava-maincraft-youtube-76.jpg'}
-        ]
+
+    _state : {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'It is my first post!', likesCount: 25},
+                {id: 2, message: 'How are you?', likesCount: 15},
+            ],
+            newPostMessage: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                {
+                    id: 1,
+                    name: 'Nick',
+                    img: 'https://pixelbox.ru/wp-content/uploads/2020/11/ava-maincraft-youtube-76.jpg'
+                },
+                {id: 2, name: 'Jhon', img: 'https://vraki.net/sites/default/files/inline/images/30_55.jpg'},
+                {id: 3, name: 'Anna', img: 'https://pixelbox.ru/wp-content/uploads/2021/04/ava-mult-vk-78.jpg'},
+            ],
+            messages: [
+                {
+                    message: 'Hi!',
+                    name: 'Nick',
+                    img: 'https://pixelbox.ru/wp-content/uploads/2020/11/ava-maincraft-youtube-76.jpg'
+                },
+                {
+                    message: 'How are you?',
+                    name: 'Nick',
+                    img: 'https://pixelbox.ru/wp-content/uploads/2020/11/ava-maincraft-youtube-76.jpg'
+                },
+                {message: 'Yo', name: 'Me', img: 'ava.jpg'},
+                {message: "I'm fine! How're you?", name: 'Me', img: 'ava.jpg'},
+            ],
+            newMessage: ''
+        },
+        friendsPage: {
+            friends: [
+                {id: 1, name: 'Jhon', img: 'https://vraki.net/sites/default/files/inline/images/30_55.jpg'},
+                {id: 2, name: 'Merry', img: 'https://pixelbox.ru/wp-content/uploads/2021/02/mult-ava-instagram-2.jpg'},
+                {id: 3, name: 'Anna', img: 'https://pixelbox.ru/wp-content/uploads/2021/04/ava-mult-vk-78.jpg'},
+                {
+                    id: 4,
+                    name: 'Nick',
+                    img: 'https://pixelbox.ru/wp-content/uploads/2020/11/ava-maincraft-youtube-76.jpg'
+                }
+            ]
+        }
+    },
+
+    addPost () {
+        if (this._state.profilePage.newPostMessage === '') {
+            return alert ('Message could not be empty!')
+        }
+        const newPost = {
+            id: this._state.profilePage.posts.length + 1,
+            message: this._state.profilePage.newPostMessage,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostMessage = '';
+        this._renderEntireTree(this._state);
+    },
+
+    updateNewPostMessage (postMessage: string) {
+        this._state.profilePage.newPostMessage = postMessage
+        this._renderEntireTree(this._state);
+    },
+
+    removePost (id: number) {
+        this._state.profilePage.posts = this._state.profilePage.posts.filter(post => post.id !== id);
+        this._renderEntireTree(this._state);
+    },
+
+    addMessage () {
+        if (this._state.dialogsPage.newMessage === '') {
+            return alert ('Message could not be empty!')
+        }
+        const newMessage = {message: this._state.dialogsPage.newMessage, name: 'Me', img: 'ava.jpg'};
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessage = '';
+        this._renderEntireTree(this._state);
+    },
+
+    updateNewMessage (message: string) {
+        this._state.dialogsPage.newMessage = message;
+        this._renderEntireTree(this._state);
     }
 }
 
-export const addPost = () => {
-    const newPost = {id: state.profilePage.posts.length + 1, message: state.profilePage.newPostMessage, likesCount: 0};
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostMessage = '';
-    renderEntireTree(state);
-}
 
-export const updateNewPostMessage = (postMessage: string) => {
-    state.profilePage.newPostMessage = postMessage
-    renderEntireTree(state);
-}
-
-export const removePost = (id: number) => {
-    state.profilePage.posts = state.profilePage.posts.filter(post => post.id !== id);
-    renderEntireTree(state);
-}
-
-export const addMessage = () => {
-    const newMessage = {message: state.dialogsPage.newMessage, name: 'Me', img: 'ava.jpg'};
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessage = '';
-    renderEntireTree(state);
-}
-
-export const updateNewMessage = (message: string) => {
-    state.dialogsPage.newMessage = message;
-    renderEntireTree(state);
-}
-
-
-export default state;
+export default store;
